3fff2e570db36978b10166eb1edc2280150495e6
jens <jens@beltman.dk>
2020-11-14 16:21:48 +0100

    Created Socket Class for function calls

---
+++ pi/client/function_call_send_socket.py
@@ -0,0 +1,13 @@
+import socket
+class FunctionCallSendSocket(socket.socket):
+    def __init__(self,HOST,PORT):
+        super().__init__(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect((HOST,PORT))
+    @staticmethod
+    def msg_format(l):
+        return bytearray(' '.join(map(str,l)),"utf8")
+
+    def motor_go(self, clockwise=False, steptype="Full",steps=200, stepdelay=.005, verbose=False, initdelay=.05):
+        self.sendall(self.msg_format(["motor_go",clockwise, steptype,steps, stepdelay, verbose, initdelay]))
+
+    
