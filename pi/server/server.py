b87c626ccbb4dde12a37176641ff716fa540288c
JensPI <jens@beltman.dk>
2020-11-14 16:24:32 +0100

    Added socket class for receiving function calls

--- pi/server/server.py
+++ pi/server/server.py
@@ -1,33 +1,7 @@
-#!/usr/bin/env python3
-
 import socket
 import RPi.GPIO as GPIO
 
-# import the library
-from RpiMotorLib import RpiMotorLib
-    
-#define GPIO pins
-GPIO_pins = (19, 26, 21) # Microstep Resolution MS1-MS3 -> GPIO Pin
-direction= 20       # Direction -> GPIO Pin
-step = 16      # Step -> GPIO Pin
+# import listen socket
+from function_call_receive_socket import FunctionCallReceiveSocket
 
-# Declare an named instance of class pass GPIO pins numbers
-mymotortest = RpiMotorLib.A4988Nema(direction, step, GPIO_pins, "A4988")
-
-HOST = '10.0.0.3'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            data_str = repr(data)
-            dl = data_str[2:-1].replace(' ','').split(',')
-            print('Received parameters ',data_str,dl)
-            mymotortest.motor_go(bool(dl[0]),dl[1] , int(dl[2]), float(dl[3]), bool(dl[4]), float(dl[5]))
-
-    
+s = FunctionCallReceiveSocket(HOST = '10.0.0.3', PORT = 65432, direction_pin=20, step_pin=16, mode_pins=(19,26,21))
