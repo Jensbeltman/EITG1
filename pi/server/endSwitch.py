90ca7593126415322602776cd2d3e25fa89f87d5
Simon Lyck Bjært Sørensen <simso16@student.sdu.dk>
2020-11-19 10:04:20 +0100

    end switch

--- pi/server/endSwitch.py
+++ pi/server/endSwitch.py
@@ -4,9 +4,9 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
-    print(GPIO.input(5), GPIO.input(4))
+    print(GPIO.input(6), GPIO.input(4))
 
