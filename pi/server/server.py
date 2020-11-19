e555d557b76dc06949d20aa890018c56bc2b6598
Simon Lyck Bjært Sørensen <simso16@student.sdu.dk>
2020-11-19 10:26:01 +0100

    end switch

--- pi/server/server.py
+++ pi/server/server.py
@@ -4,4 +4,4 @@ import RPi.GPIO as GPIO
 # import listen socket
 from function_call_receive_socket import FunctionCallReceiveSocket
 
-s = FunctionCallReceiveSocket(HOST = '10.0.0.3', PORT = 65432, direction_pin=20, step_pin=16, mode_pins=(19,26,21))
+s = FunctionCallReceiveSocket(HOST = '192.168.43.44', PORT = 65432, direction_pin=20, step_pin=16, mode_pins=(19,26,21))
