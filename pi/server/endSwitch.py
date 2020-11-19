72408aa02a16befa189156abe37f8c528179de42
Simon Lyck Bjært Sørensen <simso16@student.sdu.dk>
2020-11-19 10:21:31 +0100

    end switch

--- pi/server/endSwitch.py
+++ pi/server/endSwitch.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
 
-# GPIO6, GPIO5
+motor = RpiMotorLib.A4988Nema(20, 16, (19,26,21), "A4988")
 
 GPIO.setmode(GPIO.BCM)
 
@@ -9,4 +10,10 @@ GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while True:
     print(GPIO.input(6), GPIO.input(4))
+    if GPIO.input(6):
+        motor.motor_go(True, "Full", 1, .005, False, .05)
+    elif GPIO.input(4):
+        motor.motor_go(False, "Full", 1, .005, False, .05)
+
+
 
