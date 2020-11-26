8115a5734d0e44d5e3ff5d897ef9eedda01eb2b3
Simon Lyck Bjært Sørensen <simso16@student.sdu.dk>
2020-11-26 09:41:35 +0100

    server change

--- pi/client/client.py
+++ pi/client/client.py
@@ -1,6 +1,11 @@
 import socket
 import time
 from function_call_send_socket import FunctionCallSendSocket
+import subprocess
+
+def copy2clip(txt):
+    cmd='echo '+txt.strip()+'|clip'
+    return subprocess.check_call(cmd, shell=True)
 
 HOST = '192.168.43.44'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
@@ -8,7 +13,7 @@ PORT = 65432        # The port used by the server
 s = FunctionCallSendSocket(HOST,PORT)
 
 speed = 0.0005
-steps = 600
+steps = 100
 
 #s.motor_go(True, "Full", steps, speed, False, .05)
 #s.motor_go(False, "Full", steps, speed, False, .05)
@@ -16,6 +21,31 @@ steps = 600
 #s.motor_go(False, "Full", steps, speed, False, .05)
 #s.motor_go(True, "Full", steps, speed, False, .05)
 #s.motor_go(False, "Full", steps, speed, False, .05)
+
+
+while True:
+    intext = input("Write cmd,args: ")
+    #copy2clip(intext)
+    cmd, *args = intext.split(",")
+    print(cmd, args)
+    if cmd == "stop":
+        print("Stopping")
+        break
+
+    if cmd == "in":
+        if args == []:
+            steps = 50
+        else:
+            steps = args[0]
+        s.motor_go(False, "Full", steps, speed, False, .05)
+    if cmd == "out":
+        if args == []:
+            steps = 50
+        else:
+            steps = args[0]
+        s.motor_go(True, "Full", steps, speed, False, .05)
+
+
 
 #time.sleep(10)
 
@@ -28,7 +58,7 @@ steps = 600
 
 #s.motor_go_to_endswith(endswith="any", clockwise=False, steptype="Full", steps=25, stepdelay=speed, verbose=False, initdelay=.05)
 
-s.run_switch_test(endswith="any")
+#s.run_switch_test(endswith="any")
 
 
 s.close()
